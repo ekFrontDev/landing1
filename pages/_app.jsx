4a54ddf548161c2940a4c7cb677de421d9b36292
ekFrontDev <egors.cube@gmail.com>
2025-09-02 18:27:11 +0300

    исправил расширение файла SwiperCustom.css

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,5 @@
 import '../app/styles/global.scss';
-import '../styles/SwiperCustom.scss';
+import '../styles/SwiperCustom.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
