0063493c3411a385fac0c68381e2fd1bf807a049
ekFrontDev <egors.cube@gmail.com>
2025-09-22 19:29:33 +0300

    переделал страницу меню

--- widgets/header/ui/Header.tsx
+++ widgets/header/ui/Header.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import ArtName from '../../../components/ArtName/ArtName';
-import A from '../../../shared/ui/link/A';
 
-export default function Header() {
+export default function Header({ img, hrefBtn }) {
   return (
     <header>
       <div className="navBar">
@@ -19,8 +18,8 @@ export default function Header() {
         />
         <span className="name">ТАТЬЯНА ТАБАЧОК</span>
         {/* <A href={'/menu'} text={'МЕНЮ'} styleVisible="invisibleMenu" /> */}
-        <Link className="iconMenu" href={'/menu'}>
-          <Image className="" src="/menu.svg" width={45} height={45} alt="" />
+        <Link className="iconMenu" href={hrefBtn}>
+          <Image className="" src={img} width={45} height={45} alt="" />
         </Link>
       </div>
       <ArtName />
